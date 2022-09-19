92b10b5d24e23be395ec8b4ab0626183791bdfd3
YaniMedici <mediciyanina@gmail.com>
2022-09-18 23:35:50 -0300

    Update/ Creación de paginador

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const apiPrivate = '647db36cd1ad69c1263e719c49cc462cd0bd8519';
 const getData = () =>{
     const url = `http://gateway.marvel.com/v1/public/comics?apikey=${apiPublic}`;
     fetch(url) 
-        .then(resp => resp.json())
+    .then(resp => resp.json())
         .then(json => printData(json))
         .catch(err => console.error(err))
 
@@ -15,24 +15,53 @@ const getData = () =>{
 
 getData();
 
+
+
+
+// <!-- Paginador -->
+
+// ENTRA EN LA API
+
+let pagina = 1;
+
+const getPage = async () => {
+    const url = `https://gateway.marvel.com/v1/public/comics?offset=${pagina}&apikey=${apiPublic}`;
+    const resp = await fetch(url)
+    // console.log(url)
+    const json = await resp.json()
+    printData(json.results)
+    console.log(json)
+    return json;
+}
+
+getPage();
+
+
+
+
+
 //PINTA LAS CARDS
 const printData = json => {
-    const array = json.data.results;
+    const array = json;
+    // const array = json.data.results; // BELUUUUU LO TENIAS ASI - LO DEJO COMENTADO
     let card = '';
     array.forEach(comics => {
       const {images, title} = comics
         images.forEach(imagen => {
             const {path, extension} = imagen
-        card += `   
+            card += `   
             <div class="card mt-5 m-2 d-inline-flex" style="width: 17rem;">
                 <img src="${imagen.path}.${imagen.extension}" class="card-img-top img-style" alt="...">
                 <div class="card-body fw-bold py-2" style="height: 7rem;">
                     <p>${title}</p>
                 </div>
             </div>`
-
+            
             document.getElementById('cards').innerHTML = card
         });
     });
 
 };
+
+
+
