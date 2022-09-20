a405bd768d04c48913c2cb610357c8ba96dad311
YaniMedici <mediciyanina@gmail.com>
2022-09-19 22:30:28 -0300

    Update/ Maquetación - Página actual y total de páginas - Funcionalidad de paginador (SIN COMPLETAR - errores con peticiones)

--- app.js
+++ app.js
@@ -18,34 +18,12 @@ getData();
 
 
 
-// <!-- Paginador -->
-
-// ENTRA EN LA API
-
-let pagina = 1;
-
-const getPage = async () => {
-    const url = `https://gateway.marvel.com/v1/public/comics?offset=${pagina}&apikey=${apiPublic}`;
-    const resp = await fetch(url)
-    // console.log(url)
-    const json = await resp.json()
-    printData(json.results)
-    console.log(json)
-    return json;
-}
-
-getPage();
-
-
-
-
-
 //PINTA LAS CARDS
 const printData = json => {
-    const array = json;
-    // const array = json.data.results; // BELUUUUU LO TENIAS ASI - LO DEJO COMENTADO
+    // const array = json; // BELUUUUU LO TENIAS ASI (= json.data.results) PROBE CON (=json) PERO NO FUNCIONA - LO DEJO COMENTADO 
+    const array = json.data.results;
     let card = '';
-    array.forEach(comics => {
+    array.forEach(comics => { 
       const {images, title} = comics
         images.forEach(imagen => {
             const {path, extension} = imagen
@@ -65,3 +43,37 @@ const printData = json => {
 
 
 
+// <!-- Paginador -->
+
+// ENTRA EN LA API
+
+let pagina = 1;
+
+const getPage = async () => {
+    // EL CATCH TIRA ERROR!!!
+    // const url = `https://gateway.marvel.com/v1/public/comics?offset=${pagina}&apikey=${apiPublic}`;
+    // fetch(url) 
+    //     .then(resp => resp.json())
+    //     .then(json => {
+    //         printData(json.results)
+    //         data = json;
+    //     })
+    //     .catch(err => console.error(err))
+    // ---------------------------------------------
+    // ESTO FUNCIONA Y SE VE LA apiPrivate, PERO NO CAMBIA DE PAGINA Y MUESTRA MAS DE 20
+    // const url = `https://gateway.marvel.com/v1/public/comics?offset=${pagina}&apikey=${apiPublic}`;
+    // const resp = await fetch(url)
+    // console.log(resp)
+    // ---------------------------------------------
+    // ESTE NO FUNCIONA ENTRA EN CONFLICTO CON PRINTDATA Y GETDATA!!!
+    // const url = `https://gateway.marvel.com/v1/public/comics?offset=${pagina}&apikey=${apiPublic}`;
+    // const resp = await fetch(url)
+    // // console.log(resp)
+    // // console.log(url)
+    // const json = await resp.json()
+    // printData(json.results)
+    // console.log(json)
+    // return json;
+}
+
+getPage();
